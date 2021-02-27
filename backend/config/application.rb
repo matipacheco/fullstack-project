f89b86e7b9f301033d6bae67ce666ac826b45b54
Matías Pacheco <matipacheco@gmail.com>
2021-02-27 15:31:41 -0300

    Adding Giphy secrets to the app

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -15,5 +15,7 @@ module Backend
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    require './config/initializers/secrets_initializer'
   end
 end
