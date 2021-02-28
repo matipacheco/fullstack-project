73c741ec930514e91174f3c9bfdee232693b7428
Matías Pacheco <matipacheco@gmail.com>
2021-02-28 13:14:11 -0300

    GifService provides a way to fetch multiple (favorited) images in one request

--- backend/app/services/gif_service.rb
+++ backend/app/services/gif_service.rb
@@ -5,14 +5,20 @@ module Giphy
   class GifService
     include HTTParty
 
-    BASE_URL = 'https://api.giphy.com/v1/gifs/'
+    BASE_URL = 'https://api.giphy.com/v1/gifs'
 
     # Returns a list of gifs that match a given search term.
     # Params:
     # - q: The search term.
     # - limit: The maximum number of elements to be fetched.
     def self.search(q, limit = 10)
-      response = get(BASE_URL + 'search', { query: { q: q, limit: limit, api_key: AppCredentials[:shared][:giphy][:api_key] } })
+      response = get(BASE_URL + '/search', {
+                       query: {
+                         q: q,
+                         limit: limit,
+                         api_key: AppCredentials[:shared][:giphy][:api_key]
+                       }
+                     })
       return nil if response.code != 200
 
       response['data']
@@ -22,7 +28,22 @@ module Giphy
     # Params:
     # - gif_id: Giphy's ID of the gif.
     def self.get_gif(gif_id)
-      response = get(BASE_URL + gif_id.to_s, { query: { api_key: AppCredentials[:shared][:giphy][:api_key] } })
+      response = get(BASE_URL + "/#{gif_id}", { query: { api_key: AppCredentials[:shared][:giphy][:api_key] } })
+      return nil if response.code != 200
+
+      response['data']
+    end
+
+    # Returns the information of a group of gifs.
+    # Params:
+    # - gif_ids: List of Giphy's IDs.
+    def self.get_gifs(gif_ids)
+      response = get(BASE_URL, {
+                       query: {
+                         ids: gif_ids.join(','),
+                         api_key: AppCredentials[:shared][:giphy][:api_key]
+                       }
+                     })
       return nil if response.code != 200
 
       response['data']
--- backend/spec/services/gif_service_spec.rb
+++ backend/spec/services/gif_service_spec.rb
@@ -66,6 +66,40 @@ describe Giphy::GifService do
       it "returns nil response" do
         expect(described_class.get_gif(gif.id)).to be_nil
       end
-    end  
+    end
+  end
+
+  describe 'get_gifd method' do
+    let!(:second_gif) { create(:gif, id: 'ID2') }
+
+    context 'when API responds successfully' do
+      before do
+        stub_request(:get, 'https://api.giphy.com/v1/gifs')
+          .with(query: { ids: 'ID,ID2', api_key: AppCredentials[:shared][:giphy][:api_key]})
+          .to_return(
+            status: 200,
+            body: {
+              data: [gif, second_gif]
+            }.to_json
+          )
+      end
+
+      it "returns API's response" do
+        response = described_class.get_gifs([gif.id, second_gif.id])
+        expect(response).not_to be_empty
+      end
+    end
+
+    context 'when API responds with error' do
+      before do
+        stub_request(:get, 'https://api.giphy.com/v1/gifs')
+          .with(query: { ids: 'ID,ID2', api_key: AppCredentials[:shared][:giphy][:api_key]})
+          .to_return(status: 500)
+      end
+
+      it "returns nil response" do
+        expect(described_class.get_gifs([gif.id, second_gif.id])).to be_nil
+      end
+    end
   end
 end
